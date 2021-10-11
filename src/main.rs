d4e5079bb93f4dd546adf1ac5f49f6746928c1bc
Chugunov Roman <Zebs-BMK@yandex.ru>
2021-10-11 13:29:50 +0300

    Release 0.3.3 version

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,9 @@ fn main() {
     String::from("/home/ruut/.local/share/bad_fonts"),
   ];
 
-  let mut fonts = FontsHelper::new(&dirs);
+  let fonts = FontsHelper::new(&dirs);
 
-  let mut font = fonts.next().unwrap();
-  println!("Font entry: {}, count: {}", font.path, font.entries.len());
-
-  font = fonts.next().unwrap();
-  println!("Font entry: {}, count: {}", font.path, font.entries.len());
-
-  font = fonts.next().unwrap();
-  println!("Font entry: {}, count: {}", font.path, font.entries.len());
-
-  font = fonts.next().unwrap();
-  println!("Font entry: {}, count: {}", font.path, font.entries.len());
-
-  font = fonts.next().unwrap();
-  println!("Font entry: {}, count: {}", font.path, font.entries.len());
-
-  font = fonts.next().unwrap();
-  println!("Font entry: {}, count: {}", font.path, font.entries.len());
+  for font in fonts {
+    println!("Font entry: {}, count: {}", font.path, font.entries.len());
+  }
 }
